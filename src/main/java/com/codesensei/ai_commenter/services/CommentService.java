6cde55e8012f2edae6f6237ce7fb731610e60b38
juanan04 <ja.aragon23@gmail.com>
2025-02-28 13:13:08 +0100

    first changes in service

--- src/main/java/com/codesensei/ai_commenter/services/CommentService.java
+++ src/main/java/com/codesensei/ai_commenter/services/CommentService.java
@@ -1,5 +1,15 @@
 package com.codesensei.ai_commenter.services;
 
+import org.springframework.stereotype.Service;
+
+import com.codesensei.ai_commenter.models.CodeResponse;
+
+@Service
 public class CommentService {
-    
+
+    public CodeResponse getComment(String code, String description, String codeLanguage, String userLanguage) {
+        // TODO: Implementar llamada a API OpenAI y lógica del servicio
+        return null;
+    }
+
 }
