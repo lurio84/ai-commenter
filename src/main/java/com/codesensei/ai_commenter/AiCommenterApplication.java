8e794beab8c7e209172bbc3707418b163a109469
lurio84 <lucas.ronquillo@gmail.com>
2025-02-28 12:30:02 +0100

    first commit

---
+++ src/main/java/com/codesensei/ai_commenter/AiCommenterApplication.java
@@ -0,0 +1,13 @@
+package com.codesensei.ai_commenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCommenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCommenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesensei/ai_commenter/AiCommenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesensei.ai_commenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCommenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
