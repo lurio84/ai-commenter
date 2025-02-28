cda088cdfbbd165d028e41e30364c7a382b5fed8
juanan04 <ja.aragon23@gmail.com>
2025-02-28 13:07:33 +0100

    basic POST controller

--- src/main/java/com/codesensei/ai_commenter/controllers/CommentController.java
+++ src/main/java/com/codesensei/ai_commenter/controllers/CommentController.java
@@ -1,5 +1,22 @@
 package com.codesensei.ai_commenter.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codesensei.ai_commenter.services.CommentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
 public class CommentController {
-    
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping("/comment")
+    public CodeResponse getComment(@RequestBody String code, @RequestBody String description,
+            @RequestBody String codeLanguage, @RequestBody String userLanguage) {
+        return commentService.getComment(code, description, codeLanguage, userLanguage);
+    }
+
 }
