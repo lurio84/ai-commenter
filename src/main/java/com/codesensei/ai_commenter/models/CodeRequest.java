d85cb9fc300bd9de1b6972c2fd2f1ca32a4c343f
lurio84 <lucas.ronquillo@gmail.com>
2025-02-28 13:07:47 +0100

    Basic models

--- src/main/java/com/codesensei/ai_commenter/models/CodeRequest.java
+++ src/main/java/com/codesensei/ai_commenter/models/CodeRequest.java
@@ -1,5 +1,11 @@
 package com.codesensei.ai_commenter.models;
 
+import lombok.Data;
+
+@Data
 public class CodeRequest {
-    
+    String code; // El codigo que el usuario desea comentar
+    String description; // El usuario puede proporcionar informacion adicional sobre el codigo
+    String codeLanguage; // El lenguaje de programacion del codigo
+    String userLanguage; // El lenguaje en el que el usuario desea recibir el comentario
 }
