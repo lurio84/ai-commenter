9d16434218c27670232ed6d141dee1dd62239e2d
juanan04 <ja.aragon23@gmail.com>
2025-03-01 16:18:45 +0100

    Added OpenRouter apikey in Service and .properties

--- src/main/java/com/codesensei/ai_commenter/services/CommentService.java
+++ src/main/java/com/codesensei/ai_commenter/services/CommentService.java
@@ -1,11 +1,15 @@
 package com.codesensei.ai_commenter.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.codesensei.ai_commenter.models.CodeResponse;
 
 @Service
 public class CommentService {
+
+    @Value("${external.api.url}")
+    private String apiUrl;
 
     public CodeResponse getComment(String code, String description, String codeLanguage, String userLanguage) {
         // TODO: Implementar llamada a API OpenAI y lógica del servicio
