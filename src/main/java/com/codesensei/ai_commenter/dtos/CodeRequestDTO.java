3085d11e049a863108409d2c28b4693a5f84a52f
juanan04 <ja.aragon23@gmail.com>
2025-03-03 02:18:01 +0100

    Added swagger documentation

--- src/main/java/com/codesensei/ai_commenter/dtos/CodeRequestDTO.java
+++ src/main/java/com/codesensei/ai_commenter/dtos/CodeRequestDTO.java
@@ -1,13 +1,18 @@
 package com.codesensei.ai_commenter.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class CodeRequestDTO {
+    @Schema(description = "Code to comment", example = "public int sumar(int a, int b) { return a + b; }")
     String code; // El codigo que el usuario desea comentar
+    @Schema(description = "Additional information about the code.", example = "")
     String description; // El usuario puede proporcionar informacion adicional sobre el codigo
+    @Schema(description = "The programming language", example = "Java")
     String codeLanguage; // El lenguaje de programacion del codigo
+    @Schema(description = "The language to get the comment", example = "English")
     String userLanguage; // El lenguaje en el que el usuario desea recibir el comentario
 }
